aac22dd2561a4b258db23b7084eb746d16cac640
Giovanni De Toni <giovanni.det@gmail.com>
2017-04-18 11:28:53 +0200

    Use trusty Ubuntu.

--- script/check_format.sh
+++ script/check_format.sh
@@ -3,17 +3,17 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     BASE_COMMIT=$(git rev-parse $TRAVIS_BRANCH)
     echo "Running clang-format-3.8 against branch $TRAVIS_BRANCH, with hash $BASE_COMMIT"
     COMMIT_FILES=$(git diff --name-only $BASE_COMMIT | grep -i -v LinkDef)
-    RESULT_OUTPUT="$(git clang-format --commit $BASE_COMMIT --diff --binary `which clang-format-3.8` $COMMIT_FILES)"
+    RESULT_OUTPUT="$(git clang-format-3.8 --commit $BASE_COMMIT --diff --binary `which clang-format-3.8` $COMMIT_FILES)"
 
     if [ "$RESULT_OUTPUT" == "no modified files to format" ] \
-      || [ "$RESULT_OUTPUT" == "clang-format did not modify any files" ] ; then
-      echo "clang-format passed."
+      || [ "$RESULT_OUTPUT" == "clang-format-3.8 did not modify any files" ] ; then
+      echo "clang-format-3.8 passed."
       exit 0
     else
       echo "clang-format failed."
       echo "To reproduce it locally please run"
       echo -e "\tgit checkout $TRAVIS_BRANCH"
-      echo -e "\tgit clang-format --commit $BASE_COMMIT --diff --binary $(which clang-format)"
+      echo -e "\tgit clang-format-3.8 --commit $BASE_COMMIT --diff --binary $(which clang-format-3.8)"
       echo "$RESULT_OUTPUT"
       exit 1
     fi
