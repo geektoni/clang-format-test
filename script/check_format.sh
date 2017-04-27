4983b9bdbb1ebc7635113a358672347e643defa5
Giovanni De Toni <giovanni.det@gmail.com>
2017-04-27 11:52:37 +0200

    Add new travis conf version.

--- script/check_format.sh
+++ script/check_format.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+# This script was originally inspired by:
+# https://github.com/root-project/root/blob/master/.travis.yml
+
+if [ $TRAVIS_PULL_REQUEST != false ]; then
+
     BASE_COMMIT=$(git rev-parse $TRAVIS_BRANCH)
+
+    echo "-----"
+    echo "Shogun Style Checker"
     echo "-----"
     echo "Running clang-format-3.8 against branch $TRAVIS_BRANCH, with hash $BASE_COMMIT"
-    COMMIT_FILES=$(git diff --name-only $BASE_COMMIT | grep -i -v LinkDef)
+
+    COMMIT_FILES=$(git diff --name-only $BASE_COMMIT)
     RESULT_OUTPUT="$(git clang-format-3.8 --commit $BASE_COMMIT --diff --binary `which clang-format-3.8` $COMMIT_FILES)"
 
     if [ "$RESULT_OUTPUT" == "no modified files to format" ] \
-        || [ "$RESULT_OUTPUT" == "clang-format-3.8 did not modify any files" ] ; then
+        || [ "$RESULT_OUTPUT" == "clang-format-3.8 did not modify any files" ] \
+        || [ "$RESULT_OUTPUT" == "clang-format did not modify any files" ]; then
           echo "clang-format-3.8 passed. \o/"
           exit 0
     else
@@ -20,7 +29,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         echo -e "\t1) git checkout $TRAVIS_PULL_REQUEST_BRANCH"
         echo -e "\t2) git clang-format-3.8 --commit $BASE_COMMIT --binary $(which clang-format-3.8)"
         echo "-----"
+        echo "Style errors found:"
         echo "$RESULT_OUTPUT"
         exit 1
     fi
 fi
+
